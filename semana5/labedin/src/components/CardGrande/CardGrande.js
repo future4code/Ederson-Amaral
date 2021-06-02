ad55e198efc834a4f3e075590953c7afaf59690f
edersonamaral <ederson.rafael30@gmail.com>
2021-06-02 13:08:35 -0300

    semana5-aula18-labedin

--- semana5/labedin/src/components/CardGrande/CardGrande.js
+++ semana5/labedin/src/components/CardGrande/CardGrande.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import './CardGrande.css'
+import styled from 'styled-components';
+// import './CardGrande.css'
+
+const BigCardContainer = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+    padding: 20px 10px;
+    margin-bottom: 10px;
+    height: 200px;
+`;
+
+const CardGrandeImg = styled.img`
+    width: 70px;
+    margin-right: 10px;
+`;
+
+const CardGrandeH4 = styled.h4`
+    margin-bottom: 15px;
+`;
+
+const ContentContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-items: flex-start;
+`;
 
 function CardGrande(props) {
     return (
-        <div className="bigcard-container">
-            <img src={ props.imagem } />
-            <div>
-                <h4>{ props.nome }</h4>
+        <BigCardContainer>
+            <CardGrandeImg src={ props.imagem } />
+            <ContentContainer>
+                <CardGrandeH4>{ props.nome }</CardGrandeH4>
                 <p>{ props.descricao }</p>
-            </div>
-        </div>
-    )
+            </ContentContainer>
+        </BigCardContainer>        
+    );
 }
 
 export default CardGrande;
--- semana5/labedin/src/components/CardPequeno/CardPequeno.js
+++ semana5/labedin/src/components/CardPequeno/CardPequeno.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import './CardPequeno.css'
+import styled from 'styled-components';
+// import './CardPequeno.css'
+
+const CardPequenoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    border: 1px solid black;
+    margin-bottom: 10px;
+    height: 100px;
+`;
+
+const LineContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    padding: 10px;
+`;
+
+const CardPequenoImagem = styled.img`
+    width: 40px;
+    margin-right: 10px;
+`;
+
+const CardPequenoH4 = styled.h4`
+`;
+
+const CardPequenoP = styled.p`
+`;
 
 function CardPequeno(props) {
     return (
-        <div className="smallcard-container">
-            <img src={ props.imagem } />
-            <div>
-                <h4>{ props.nome }</h4>
-            </div>
-            <div>
-                <p>{ props.descricao }</p>
-            </div>
-        </div>
+        <CardPequenoContainer>
+            <LineContainer>
+                <CardPequenoImagem src={ props.imagem } />           
+                <CardPequenoH4>{ props.nome }</CardPequenoH4>            
+                <CardPequenoP>{ props.descricao }</CardPequenoP>
+            </LineContainer>
+        </CardPequenoContainer>
     )
 }
 
