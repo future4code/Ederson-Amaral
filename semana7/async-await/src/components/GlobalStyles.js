e937568a881c706824c1fe2c699125247befaf65
edersonamaral <ederson.rafael30@gmail.com>
2021-06-16 16:14:15 -0300

    semana7-aula26-labenusers

---
+++ semana7/async-await/src/components/GlobalStyles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        border: 0;
+        vertical-align: baseline;
+        list-style: none;
+        box-sizing: border-box;
+    }
+`;
+
+export default GlobalStyle;
---
+++ semana7/async-await/src/components/TelaListaUsuario.js
@@ -0,0 +1,76 @@
+import axios from 'axios';
+import React from 'react';
+import styled from 'styled-components';
+
+const CardUsuario = styled.div`
+    border: 1px solid black;
+    padding: 10px;
+    margin: 10px;
+    width: 300px;
+    justify-content: space-between;
+    display: flex;
+`;
+
+class TelaListaUsuarios extends React.Component {
+    state = {
+        usuarios: []
+    }
+
+    componentDidMount() {
+        this.pegarUsuarios()
+    }
+
+    pegarUsuarios = async () => {
+        const url = 'https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users'
+
+        try {
+            const res = await axios.get(url, {
+                headers: {
+                    Authorization: 'ederson-amaral-molina'
+                }
+            })
+            this.setState({ usuarios: res.data })
+
+        } catch (err) {
+            alert('Ocorreu um problema, tente novamente!')
+        }
+    }
+
+
+    deletarUsuario = (id) => {
+        const url = `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`
+        axios.delete(url, {
+            headers: {
+                Authorization: 'ederson-amaral-molina'
+            }
+        })
+            .then((res) => {
+                alert('Usuário(a) deletado(a) com sucesso!')
+                this.pegarUsuarios()
+            })
+            .catch((err) => {
+                alert('Ocorreu um erro, tente novamente!')
+            })
+    }
+
+    render() {        
+        
+        const listaUsuarios = this.state.usuarios.map((user) => {
+            return (                                                               
+            <CardUsuario key={user.id}>
+                {user.name}
+                <button onClick={() => this.deletarUsuario(user.id)}>X</button>
+            </CardUsuario>                                 
+            )
+        })
+
+        return (
+            <div>                
+                <button className='button' onClick={this.props.irParaCadastro}>Ir para Cadastro</button>                
+                <h2>Lista de Usuários</h2>
+                {listaUsuarios}
+            </div>
+        )
+    }
+}
+export default TelaListaUsuarios;
