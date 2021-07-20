5b4d161cb17cc4f7141be2afb54e3321fb84668c
edersonamaral <ederson.rafael30@gmail.com>
2021-07-20 16:53:14 -0300

    criado formulário de Login na LoginPage e criado arquivo LoginForm para desestruturação

--- semana12/labeddit/src/pages/LoginPage/LoginPage.js
+++ semana12/labeddit/src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import { ScreenContainer, SignUpButtonContainer } from './styled'
+import Button from '@material-ui/core/Button'
+import LoginForm from './LoginForm'
+import { useHistory } from 'react-router-dom'
+import { goToRegister } from '../../routes/coordinator'
 
 const LoginPage = () => {
+    const history = useHistory()
     return (
-        <div>
-            <h1>LoginPage</h1>            
-        </div>
+        <ScreenContainer>
+           <LoginForm/>
+            <SignUpButtonContainer>
+                <Button
+                    type={'submit'}
+                    fullWidth
+                    variant={'text'}
+                    color={'primary'}
+                    margin={'normal'}
+                    onClick={() => goToRegister(history)}
+                >
+                    Cadastrar
+                </Button>
+            </SignUpButtonContainer>
+        </ScreenContainer>
 
     )
 }
