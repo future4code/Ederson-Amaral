72784ca0b4f942c026e58023e426603361464763
edersonamaral <ederson.rafael30@gmail.com>
2021-07-20 11:46:09 -0300

    ccriação de rotas, correção de nomes nas páginas, criado página de erros

---
+++ semana12/labeddit/src/routes/Router.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import LoginPage from '../pages/LoginPage/LoginPage'
+import PostPage from '../pages/PostPage/PostPage'
+import FeedPage from '../pages/FeedPage/FeedPage'
+import RegisterPage from '../pages/RegisterPage/RegisterPage'
+import ErrorPage from '../pages/ErrorPage/ErrorPage'
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/'>
+                    <LoginPage />
+                </Route>
+                <Route exact path='/register'>
+                    <RegisterPage />
+                </Route>
+                <Route exact path='/feed'>
+                    <FeedPage />
+                </Route> 
+                <Route exact path='/post'>
+                    <PostPage />
+                </Route>                               
+                <Route>
+                    <ErrorPage />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Router
